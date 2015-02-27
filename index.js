ff7afe48e8b82d1d8709cd9ada17358a88720c15
Pavel Trehubau <pavel.trehubau@gmail.com>
2015-02-27 15:01:12 +0300

    Added support of dust partials

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 var path = require('path');
 var dust = require('dustjs-linkedin');
 
-module.exports = function(content) {
-  if (this.cacheable) {
-    this.cacheable();
-  }
+module.exports = function (content) {
+    if (this.cacheable) {
+        this.cacheable();
+    }
 
+    var partials = [];
+    var partialRegExp = /\{>[^"]*"([^"]*)/g;
+    var match;
 
-  var name = this.resourcePath.replace(this.options.context + path.sep, '').replace('.dust', '').split(path.sep).join('/'),
-    compiled = dust.compile(content, name);
+    while (match = partialRegExp.exec(content)) {
+        partials.push(match[1]);
+    }
 
-  return "module.exports = " + compiled;
+    var dependencies = partials.filter(function (partial) {
+        return partial.indexOf('{') < 0;
+    }).reduce(function (result, partial) {
+        return result + "require('" + partial + ".dust');\n";
+    }, '');
+
+    var name = this.resourcePath.replace(this.options.context + path.sep, '').replace('.dust', '').split(path.sep).join('/'),
+        compiled = dust.compile(content, name);
+
+    return dependencies + "module.exports = " + compiled;
 };
